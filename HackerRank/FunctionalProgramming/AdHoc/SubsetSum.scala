01560ffb5ad8ac962afc667c43c4f892db8e7e0a
linxiaohui <llinxiaohui@126.com>
2017-03-23 09:25:18 +0800

    HackerRank/FunctionalProgramming/AdHoc/SubsetSum A AC solution: Data Structure Matters!!

--- HackerRank/FunctionalProgramming/AdHoc/SubsetSum.scala
+++ HackerRank/FunctionalProgramming/AdHoc/SubsetSum.scala
@@ -1,18 +1,18 @@
 /*
-ven a list of N positive integers, A = {a[1], a[2], ..., a[N]} and another integer S,
+given a list of N positive integers, A = {a[1], a[2], ..., a[N]} and another integer S,
 find whether there exists a non-empty subset of A whose sum is greater than or equal to S. 
  */
 
-package FunctionalProgramming.Recursion
+package FunctionalProgramming.AddHoc
 object SubsetSum {
 
-    def FindG(L:List[Int], V:Int, depth:Int):Int = {
+    def FindG(L:Array[Int], V:Int, depth:Int):Int = {
         val t=L.takeWhile(_<V)
         if(t.length==L.length) -1
         else t.length
     }
 
-    def BinFind(L:List[Int], V:Int, beg:Int, end:Int):Int = {
+    def BinFind(L:Array[BigInt], V:BigInt, beg:Int, end:Int):Int = {
         if(L(end)<V) {
             -1
         }
@@ -34,11 +34,11 @@ object SubsetSum {
 
     def main(args: Array[String]) {
         val N=readInt
-        val A=readLine().trim.split(" ").map(_.toInt).toList
-        val sum=A.sorted.reverse.foldLeft(List(0)) { case(x,y)=> x++List(x.last+y)}
+        val A=readLine().trim.split(" ").map(_.toInt)
+        val sum=A.sorted.reverse.scanLeft(0:BigInt)((x,y)=>x+y)
         val T=readInt
         (1 to T).foreach(_=> {
-            val S=readInt
+            val S=BigInt(readLine)
             //println(FindG(sum,S,1))
             println(BinFind(sum, S, 0, sum.length-1))
         })

643728a0c1400ab0aa9d71529f1a038207804ef5
linxiaohui <llinxiaohui@126.com>
2017-03-23 09:16:43 +0800

    HackerRank/FunctionalProgramming/Recursion/PrefixCompression fix

--- HackerRank/FunctionalProgramming/Recursion/PrefixCompression.scala
+++ HackerRank/FunctionalProgramming/Recursion/PrefixCompression.scala
@@ -6,18 +6,26 @@
 package FunctionalProgramming.Recursion
 object PrefixCompression {
 
+    /*prequire: x.length==y.length*/
     def FindPrefix(x:String, y:String):String = {
-        if(x.length>0 && y.length>0 && x.head==y.head) {
-            x.head+FindPrefix(x.tail,y.tail)
+        if(x.length==1) {
+            if(x==y) x else ""
         }
         else {
-            ""
+            val mid=x.length/2
+            if(x.substring(0,mid)==y.substring(0,mid)) {
+                x.substring(0,mid)+FindPrefix(x.substring(mid),y.substring(mid))
+            }
+            else {
+                FindPrefix(x.substring(0,mid),y.substring(0,mid))
+            }
         }
     }
     def main(args: Array[String]) {
         val x=readLine
         val y=readLine
-        val comm=FindPrefix(x,y)
+        val m=List(x.length,y.length).min
+        val comm=FindPrefix(x.substring(0,m),y.substring(0,m))
         println(comm.length+" "+comm)
         println(x.length-comm.length+" "+x.substring(comm.length))
         println(y.length-comm.length+" "+y.substring(comm.length))

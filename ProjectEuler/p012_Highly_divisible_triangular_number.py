5625b38f1716362e9e503eeb2bddcd22ee42b3ac
linxiaohui <llinxiaohui@126.com>
2017-03-23 08:44:58 +0800

    ProjectEuler/p012,p014优化

--- ProjectEuler/p012_Highly_divisible_triangular_number.py
+++ ProjectEuler/p012_Highly_divisible_triangular_number.py
@@ -5,20 +5,28 @@ import timeit
 def P012():
     import math
     def divisors(n):
+        l=2
         r=int(math.sqrt(n))+1
-        s=set([1,n])
         for i in range(2,r):
             if n%i==0:
-                s.add(i)
-                s.add(n/i)
-        return len(s)
+                l+=2
+            if i*i==n:
+                l-=1
+        return l
     n=10
     while True:
-        l=divisors(n*(n+1)/2)
-        if l>500:
-            print n*(n+1)/2
-            break
+        if n%2==0:
+            if divisors(n/2)*divisors(n+1)>500:
+                print n*(n+1)/2
+                break
+        else:
+            if divisors((n+1)/2)*divisors(n)>500:
+                print n*(n+1)/2
+                break
         n=n+1
 
-
 print timeit.timeit(P012, number=1)
+
+"""As n and n+1 have no factors in common (except for the number 1)
+one can multiply the factor counts in n/2 and n+1, or n and (n+1)/2 as,
+to arrive at the factor count of the nth triangle number."""
--- ProjectEuler/p014_Longest_Collatz_sequence.py
+++ ProjectEuler/p014_Longest_Collatz_sequence.py
@@ -3,14 +3,24 @@
 import timeit
 
 def P014():
-    def Collatz(n):
+    collatz=[0]*(1000000+1)
+    collatz[1]=1
+    def Collatz(k):
         r=1
+        n=k
         while n!=1:
             if n%2==0:
                 n=n/2
+                if n<1000000 and collatz[n]!=0:
+                    collatz[k]=collatz[n]+r
+                    return collatz[k]
             else:
                 n=3*n+1
+                if n<1000000 and collatz[n]!=0:
+                    collatz[k]=collatz[n]+r
+                    return collatz[k]
             r+=1
+        collatz[k]=r
         return r
     longest=(1,1)
     for i in range(2,1000000+1):

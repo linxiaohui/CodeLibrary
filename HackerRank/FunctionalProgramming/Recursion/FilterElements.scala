2efae956324dc5fcd79a03258b6ff20ab3220b19
linxiaohui <llinxiaohui@126.com>
2017-03-23 09:11:04 +0800

    HackerRank/FunctionalProgramming/Recursion/FilterElements a FUNCTIONAL solution

--- HackerRank/FunctionalProgramming/Recursion/FilterElements.scala
+++ HackerRank/FunctionalProgramming/Recursion/FilterElements.scala
@@ -6,38 +6,41 @@ find those integers which are repeated at least K times. In case no such element
 
 package FunctionalProgramming.Recursion
 object FilterElements {
+
+    def Filterer(l:List[Int], m:Map[Int,Int], k:Int, e:List[Int]):List[Int] ={
+        if(l.length>0) {
+            val first=l.head
+            if(m(first)>=k && !e.contains(first)) {
+                first::Filterer(l.tail, m, k, first::e)
+            }
+            else {
+                Filterer(l.tail, m, k, e)
+            }
+        }
+        else {
+            Nil
+        }
+    }
     def main(args:Array[String]) {
         val T=readInt
-        (1 to T).foreach(_=>{
+        (1 to T).foreach((z:Int)=>{
+            //readInt() reads the whole line and try to parse the integer from it
+            //val N=readInt
+            //val T=readInt
             val X=readLine().trim().split(" ").map(x=>x.toInt).toList
             val N=X(0)
             val T=X(1)
             val S=readLine().trim().split(" ").map(x=>x.toInt).toList
-            var m=new scala.collection.immutable.HashMap[Int,Int]
-            S.foreach(d=>{
-                if(m.contains(d))
-                    m+=((d,m(d)+1))
-                else
-                    m+=((d,1))
-            })
-            var l=List[Int]()
-            val o=S.filter(d=>{
-                if(!l.contains(d) && m(d)>=T) {
-                    l=d::l
-                    true
-                }
-                else {
-                    false
-                }
-            })
-            o.foreach(d=>{
-                print(d)
-                print(" ")
-            })
+            //val m=S.map((d:Int)=>(d,1)).groupBy(_._1).map { case(x,y)=>(x,y.length) }
+            //val m=S.map(_=>(_,1)).groupBy(_._1).map { case(x,y)=>(x,y.length) }: ERROR
+            val m=S.map((_,1)).groupBy(_._1).map { case(x,y)=>(x,y.length) }
+            val o=Filterer(S,m,T,List[Int]())
             if(o.length==0) {
-                print(-1)
+                println(-1)
             }
-            println
+            else {
+                println(o.map(_.toString).mkString(" "))
+            }
         })
     }
 }

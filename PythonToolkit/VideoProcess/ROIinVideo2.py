6cc72d945263e285bfc5664403b7ab92b7bbb01b
linxiaohui <llinxiaohui@126.com>
2017-08-04 01:04:17 +0800

    ROI in Video Using moviepy

--- PythonToolkit/VideoProcess/ROIinVideo2.py
+++ PythonToolkit/VideoProcess/ROIinVideo2.py
@@ -1,30 +1,25 @@
 # -*- coding: utf-8 -*-
 
+import sys
 import cv2
 from moviepy.editor import *
 
 def VideoInfo(inpath, outpath, sec=10):
-    inputv = cv2.VideoCapture(inpath)
-    i=0
-    fps = int(inputv.get(cv2.CAP_PROP_FPS))
-    width, height = int(inputv.get(cv2.CAP_PROP_FRAME_WIDTH)), int(inputv.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    inputv = VideoFileClip(inpath)
+    fps = inputv.fps
+    width, height = inputv.size
     print("fps={}".format(fps))
     print("width={}, height={}".format(width, height))
-    frame = None
-    while i<=sec*fps:
-        preframe = frame
-        grabbed, frame = inputv.read()
-        if not grabbed:
-            break
-        i+=1
+    #get_frame return RGB ?
+    preframe = inputv.get_frame(sec)[:,:,[2,1,0]]
     cv2.namedWindow("info")
     def onMouse(evt, x, y, flag, param):
         if flag & cv2.EVENT_FLAG_LBUTTON:
             param["pos"]=(x,y)
             showframe = preframe.copy()
             height, width,color = showframe.shape
-            showframe = cv2.line(showframe, (x,0),(x,height), (0,255,0),2)
-            showframe = cv2.line(showframe, (0,y),(width,y), (0,0,255),2)
+            showframe = cv2.line(showframe, (x,0),(x,height), (0,255,0),1)
+            showframe = cv2.line(showframe, (0,y),(width,y), (0,0,255),1)
             cv2.putText(showframe, "x={},y={}".format(x,y), (width//2, height//2), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 255), 2)
             cv2.imshow("info", showframe)
             kvalue = cv2.waitKey(0)
@@ -43,23 +38,24 @@ def VideoInfo(inpath, outpath, sec=10):
     else:
         return None
 
-def SliceROI(inpath, roi, outpath):
+def SliceROI(inpath, roi, outpath, start=0):
     def get_roi(image):
         x,y,w,h=roi
         return image[y:y+h, x:x+w]
-    clip = VideoFileClip(inpath)
+    clip = VideoFileClip(inpath).subclip(start)
     print("fps={}; duration={}".format(clip.fps, clip.duration))
     roiclip = clip.fl_image(get_roi)
     roiclip.write_videofile(outpath)
 
 def main():
-    inpath = r"f:\x.mp4"
-    outpath = r"f:\out.mp4"
-    roi = VideoInfo(inpath, outpath)
+    inpath = sys.argv[1]
+    outpath = sys.argv[2]
+    print("Converting {} to {}".format(inpath, outpath))
+    roi = VideoInfo(inpath, outpath,sec=900)
     if roi is None:
         print("DID NOT SELECT ROI. Exiting...")
         return
-    SliceROI(inpath, roi, outpath)
+    SliceROI(inpath, roi, outpath, start=120)
     print("Done")
 
 def getinfo():

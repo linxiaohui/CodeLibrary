c4e551e89a24b8907803056449d2f08082a57ea7
linxiaohui <llinxiaohui@126.com>
2017-09-16 22:42:52 +0800

    WebVTT字幕转srt字幕【使用ffmpeg】

--- PythonToolkit/WebVTT2srt.py
+++ PythonToolkit/WebVTT2srt.py
@@ -5,15 +5,12 @@ WebVTT字幕转化为SRT字幕
 import sys
 import os
 import glob
-
+from moviepy.config import get_setting
 
 for f in glob.iglob(os.path.join(sys.argv[1], "**"), recursive=True):
     if f.lower().endswith('.vtt'):
         srt=f[:-4]+".srt"
-        print(srt)
-        with open(f,"rb") as fp:
-            fw=open(srt,"wb")
-            for line in fp:
-                if line.strip()!=b"WEBVTT":
-                    fw.write(line)
-            fw.close()
+        cmd = [get_setting("FFMPEG_BINARY"), "-i", '"{}"'.format(f), '"{}"'.format(srt)]
+        cmdline = ' '.join(cmd)
+        print(cmdline)
+        os.system(cmdline)

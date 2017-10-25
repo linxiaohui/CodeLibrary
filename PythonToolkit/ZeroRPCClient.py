a76d39168b5c931f6832c7a1315eefea38045dbc
linxiaohui <llinxiaohui@126.com>
2017-10-25 17:05:57 +0800

    ZeroRPC/Server&Client

---
+++ PythonToolkit/ZeroRPCClient.py
@@ -0,0 +1,22 @@
+﻿# -*- coding: utf-8 -*-
+
+import zerorpc
+import time
+
+s = zerorpc.Client()
+s.connect("tcp://127.0.0.1:1234")
+
+print(s.Hello("World"))
+print(s.inc(100))
+
+CNT=100
+start=time.time()
+for i in range(CNT):
+    s.Hello("World")
+end=time.time()
+print(CNT/(end-start))
+start=time.time()
+for i in range(CNT):
+    s.inc(123)
+end=time.time()
+print(CNT/(end-start))
---
+++ PythonToolkit/ZeroRPCServer.py
@@ -0,0 +1,13 @@
+﻿# -*- coding: utf-8 -*-
+
+import zerorpc
+
+class Services:
+    def Hello(self, name):
+        return "Hello [{}]".format(name)
+    def inc(self, n):
+        return n + 1
+
+s = zerorpc.Server(Services())
+s.bind("tcp://0.0.0.0:1234")
+s.run()

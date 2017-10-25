923bab2fe7aa878d6dae1d888e65ea6b6ea98bd7
linxiaohui <llinxiaohui@126.com>
2017-10-25 17:16:59 +0800

    xmlRPC/Server&Client

---
+++ PythonToolkit/xmlRPCClient.py
@@ -0,0 +1,25 @@
+﻿# -*- coding: utf-8 -*-
+
+import time
+
+try:
+    from xmlrpclib import ServerProxy
+except:
+    from xmlrpc.client import ServerProxy
+
+s = ServerProxy("http://127.0.0.1:1234")
+
+print(s.Hello("World"))
+print(s.inc(100))
+
+CNT=100
+start=time.time()
+for i in range(CNT):
+    s.Hello("World")
+end=time.time()
+print(CNT/(end-start))
+start=time.time()
+for i in range(CNT):
+    s.inc(123)
+end=time.time()
+print(CNT/(end-start))
---
+++ PythonToolkit/xmlRPCServer.py
@@ -0,0 +1,19 @@
+﻿# -*- coding: utf-8 -*-
+
+try:
+    from xmlrpc.server import SimpleXMLRPCServer
+except:
+    from SimpleXMLRPCServer import SimpleXMLRPCServer
+
+
+def Hello(name):
+    return "Hello [{}]".format(name)
+
+def inc(n):
+    return n + 1
+
+s = SimpleXMLRPCServer(('0.0.0.0',1234))
+s.register_function(Hello)
+s.register_function(inc)
+s.serve_forever()
+
